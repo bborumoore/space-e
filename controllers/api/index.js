480d08ab0101267c1cc8057811286783c4d6645b
Claire Phillips <claireacam@gmail.com>
2021-05-05 21:15:05 -0400

    Several major fixes
    1. Fixed homepage button
    2. Logging in now takes you to profile
    3. Can now update preferences
    4. Can now create new users from login page's sign-up form (fixed sign-up form)
       Note: New user's password is not encrypted before adding to database, need to add encryption
       Note: Since password is not encrypted, new users cannot login after logging out
    5. Creating new user also creates preferences for that user
    6. Added preferences route
    7. Updated some user and home routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 
 const userRoutes = require('./users');
+const prefRoutes = require('./preferences');
+
 router.use('/users', userRoutes);
+router.use('/preferences', prefRoutes);
 
 module.exports = router;
--- public/js/login.js
+++ public/js/login.js
@@ -22,12 +22,12 @@ const loginFormHandler = async (event) => {
     }
   };
   
-  const signupFormHandler = async (event) => {
+const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    const name = document.querySelector('#name-signup')
-    const email = document.querySelector('#email-signup')
-    const password = document.querySelector('#password-signup')
+    const name = document.querySelector('#name-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
   
     if (name && email && password) {
       const response = await fetch('/api/users', {

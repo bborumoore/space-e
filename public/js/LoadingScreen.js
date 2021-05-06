0beb52cb4414a16ad88d54675b3c001b61ef57e6
Valerie Russell <vruss14@gmail.com>
2021-05-06 15:59:05 -0400

    deleted static HTML files, removed unused code, minor CSS adjustments

--- public/js/LoadingScreen.js
+++ public/js/LoadingScreen.js
@@ -6,27 +6,22 @@ function setTime() {
     secondsRemaining--;
 
     if(secondsRemaining === 4) {
-        console.log(secondsRemaining);
         loadingBar.setAttribute("aria-valuenow", "25");
         loadingBar.setAttribute("style", "width: 25%;");
 
     } else if (secondsRemaining === 3) {
-        console.log(secondsRemaining);
         loadingBar.setAttribute("aria-valuenow", "50");
         loadingBar.setAttribute("style", "width: 50%;");
 
     } else if (secondsRemaining === 2) {
-        console.log(secondsRemaining);
         loadingBar.setAttribute("aria-valuenow", "75");
         loadingBar.setAttribute("style", "width: 75%;");
 
     } else if (secondsRemaining === 1) {
-        console.log(secondsRemaining);
         loadingBar.setAttribute("aria-valuenow", "100");
         loadingBar.setAttribute("style", "width: 100%;");
 
     } else if (secondsRemaining === 0) {
-        console.log(secondsRemaining);
         clearInterval(timerInterval);
         // At this point, redirect to the home page
         window.location.href = '/home'
